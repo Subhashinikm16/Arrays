34797232ad2c7f0c87ff11d14cb4953b9de16f5a
Subhashinikm16 <112954382+Subhashinikm16@users.noreply.github.com>
2022-09-09 10:43:18 +0530

    Add files via upload

---
+++ 5.Array Sort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner ob=new Scanner(System.in);
+		int n=ob.nextInt();
+		int i,j;
+		int a[]=new int[n];
+		for(i=0;i<n;i++){
+		    a[i]=ob.nextInt();
+		}
+		Arrays.sort(a);
+		int h=n/2;
+
+		for(i=h,j=h+1;i>=0;i--,j++){
+		if(i==0 && j<n-1){
+		        System.out.print(a[i]);
+		}
+		else{
+		    System.out.print(a[i]+" ");
+		    System.out.print(a[j]+" ");
+		}
+		}
+	}
+}
